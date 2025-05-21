32c147a3631d10c09337dfdd1a7d9c51e2d07a5f
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-05-21 20:16:26 +0300

    добавлена общая логика для всех игр в index.js

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import readlineSync from 'readline-sync'
+
+const app = function (gameDescription, gameRules, rightAnswer) {
+  // Приветствие
+  console.log('Welcome to the Brain Games!')
+  const name = readlineSync.question('May I have your name? ')
+  console.log(`Hello, ${name}!`)
+  console.log(gameDescription)
+
+  // Цикл
+  for (let roundCount = 0; roundCount < 3;) {
+    const hiddenNumber = gameRules()
+    console.log(`Question: ${hiddenNumber}`)
+    const playerAnswer = readlineSync.question('Your answer: ').toLowerCase()
+
+    // Проверка ответа игрока
+    if (playerAnswer === rightAnswer(hiddenNumber)) {
+      console.log('Correct!')
+      roundCount += 1
+    }
+    else {
+      console.log(
+        `"${playerAnswer}" is wrong answer ;(. Correct answer was "${rightAnswer(
+          hiddenNumber,
+        )}". Let's try again, ${name}!`,
+      )
+      roundCount = 0
+      break
+    }
+
+    // Победа игрока
+    if (roundCount === 3) {
+      console.log(`Congratulations, ${name}!`)
+    }
+  }
+}
+
+export default app
