3984dcdaee2cf314553cfa8161a3c7e4268fb04c
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-05-22 14:25:43 +0300

    Добавлен скрипт игры gcd.js - поиск наибольшего общего делителя

---
+++ src/games/gcd.js
@@ -0,0 +1,27 @@
+import app from '../index.js'
+
+// описание игры
+const gameDescription = 'Find the greatest common divisor of given numbers.'
+
+// механика игры
+const getNumbers = () => {
+  return `${Math.floor(Math.random() * 100)} ${Math.floor(Math.random() * 100)}`
+}
+
+// правильный ответ
+const rightAnswer = (numbersPair) => {
+  const numbers = numbersPair.split(' ')
+  let firstNumber = Number(numbers[0])
+  let secondNumber = Number(numbers[1])
+  // алгоритм евклида для поиска НОД
+  while (secondNumber !== 0) {
+    const temp = secondNumber
+    secondNumber = firstNumber % secondNumber
+    firstNumber = temp
+  }
+  return String(firstNumber) // преобразуем в строку, чтобы затем сравнить со строковым ответом игрока
+}
+
+rightAnswer(getNumbers())
+
+export default () => app(gameDescription, getNumbers, rightAnswer)
