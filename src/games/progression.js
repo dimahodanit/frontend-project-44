eeca156f2162e7a03c6e798e8f94885f8368c385
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-05-23 21:57:20 +0300

    добавлен скрипт игры brain-progression

---
+++ src/games/progression.js
@@ -0,0 +1,54 @@
+import app from '../index.js'
+
+// описание игры
+const gameDescription = 'What number is missing in the progression?'
+
+// механика игры
+const getProgressionNumbers = () => {
+  const progression = {
+    length: Math.floor(Math.random() * (10 - 5 + 1) + 5), // длинна прогрессии случайно число от 5 до 10 включительно
+    step: Math.floor(Math.random() * 5 + 1), // шаг прогрессии случайное число от 1 до 5 включительно.
+    start: Math.floor(Math.random() * 10), // число с которого начинается прогрессия
+    numbers: [],
+  }
+
+  // добавляем числа в массив
+  for (let index = 0; index < progression['length']; index++) {
+    const currentElement = progression['start'] + index * progression['step']
+    progression['numbers'].push(currentElement)
+  }
+  // добавляем в массив пропущенное число
+  const missNumber = '..'
+  progression['numbers'][Math.floor(Math.random() * progression['length'])]
+    = missNumber
+  return progression['numbers'].join(' ')
+}
+
+// правильный ответ
+const getRightAnswer = (progressionNumbers) => {
+  const numbersArray = progressionNumbers.split(' ')
+
+  const startIndex = 0
+  const endIndex = numbersArray.length - 1
+  const missNumberIndex = numbersArray.indexOf('..')
+  let step = Number(numbersArray[2]) - Number(numbersArray[1])
+  let rightAnswer
+
+  switch (missNumberIndex) {
+    case startIndex:
+      rightAnswer = Number(numbersArray[1]) - step
+      break
+    case endIndex:
+      rightAnswer = Number(numbersArray[endIndex - 1]) + step
+      break
+    default:
+      step
+        = (Number(numbersArray[endIndex]) - Number(numbersArray[startIndex]))
+          / (endIndex - startIndex)
+      rightAnswer = Number(numbersArray[missNumberIndex - 1]) + step
+  }
+
+  return String(rightAnswer)
+}
+
+export default () => app(gameDescription, getProgressionNumbers, getRightAnswer)
