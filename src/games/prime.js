cd9e812f753443138292b8fa8e4f8b2209017839
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-05-24 10:57:27 +0300

    добавлен скрипт игры brain-prime prime.js

---
+++ src/games/prime.js
@@ -0,0 +1,30 @@
+import app from '../index.js'
+
+// описание игры
+const gameDescription
+  = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+// механика игры
+const getRandomNumber = () => {
+  return Math.floor(Math.random() * 100)
+}
+
+// правильный ответ
+const isPrime = (hiddenNumber) => {
+  const n = hiddenNumber
+  let result = true
+  if (n < 2 || (n > 2 && n % 2 === 0)) {
+    result = false
+  }
+  else {
+    for (let i = 3; i <= n ** 0.5; i += 2) {
+      if (n % i === 0) {
+        result = false
+      }
+    }
+  }
+  const rightAnswer = result ? 'yes' : 'no'
+  return rightAnswer
+}
+
+export default () => app(gameDescription, getRandomNumber, isPrime)
