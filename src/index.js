536fd100b151385bd2116ba3e491faaa95910e89
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-05-27 09:32:20 +0500

    обеденены два параметра в один gameRules

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync'
 
-const app = function (gameDescription, gameRules, rightAnswer) {
+const app = function (gameDescription, gameRules) {
   // Приветствие
   console.log('Welcome to the Brain Games!')
   const name = readlineSync.question('May I have your name? ')
@@ -9,21 +9,18 @@ const app = function (gameDescription, gameRules, rightAnswer) {
 
   // Цикл
   for (let roundCount = 0; roundCount < 3;) {
-    const hiddenNumbers = gameRules()
-    console.log(`Question: ${hiddenNumbers}`)
+    const numbers = gameRules.numbers()
+    console.log(`Question: ${numbers}`)
     const playerAnswer = readlineSync.question('Your answer: ').toLowerCase()
 
     // Проверка ответа игрока
-    if (playerAnswer === rightAnswer(hiddenNumbers)) {
+    if (playerAnswer === gameRules.rightAnswer(numbers)) {
       console.log('Correct!')
       roundCount += 1
     }
     else {
       console.log(
-        `"${playerAnswer}" is wrong answer ;(. Correct answer was "${rightAnswer(
-          hiddenNumbers,
-        )}". Let's try again, ${name}!`,
-      )
+        `"${playerAnswer}" is wrong answer ;(. Correct answer was "${gameRules.rightAnswer(numbers)}". Let's try again, ${name}!`)
       break
     }
     // Победа игрока
