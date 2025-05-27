87a124ada8c925ad0b148b1be668bf6390dd49e1
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-05-27 09:29:19 +0500

    добавлена функция объединения правил игры в готовый набор и экспортирована

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,12 @@
 function getRandomNumber(power = 10) {
   return Math.floor(Math.random() * power)
 }
+
+function getGameRules(numbers, rightAnswer) {
+  const gameRules = {
+    numbers,
+    rightAnswer,
+  }
+  return gameRules
+}
+export { getGameRules, getRandomNumber }
